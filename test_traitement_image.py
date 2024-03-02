d64d5378afbe4725c5c00b95c96e8e8f2b3516d5
Louis2100 <louis.bodin.1@ens.etsmtl.ca>
2024-03-02 13:17:12 -0500

    02/03

--- test_traitement_image.py
+++ test_traitement_image.py
@@ -1,45 +1,77 @@
 import numpy as np
+from traitement_image import *
+def test_appliquer_transformation_1_a(img_array):
 
-img_array = [[2,5,3,9,15],
-             [6,7,9,1,5],
-             [3,8,4,2,9],
-             [2,3,5,8,2],
-             [1,2,3,2,1]]
-# Obtenir les dimensions du tableau
-nombre_lignes = len(img_array)
-nombre_colonnes = len(img_array[0])
-tableau_temp = np.zeros((nombre_lignes+1,nombre_colonnes+1))
-#print(img_array)
-#print(nombre_lignes, nombre_colonnes)
-#print(tableau_temp)
-# Pour chaque pixel, on extrait ses valeurs des voisins (V=8).
+    # Obtenir les dimensions du tableau
+    nombre_lignes = len(img_array)
+    nombre_colonnes = len(img_array[0])
+    tableau_temp = np.zeros((nombre_lignes, nombre_colonnes))
+    resultat = 0
 
-for i in range(nombre_lignes-2):
+    # Pour chaque pixel, on extrait ses valeurs des voisins (V=8).
 
-    for j in range(nombre_colonnes-2):
+    for i in range(1, nombre_lignes - 1):
 
-        valeur_pixel_central = img_array[i+1][j+1]
+        for j in range(1, nombre_colonnes - 1):
 
-        tableau_temp2 = [[img_array[i][0]], [img_array[0][j+1]], [img_array[0][j+2]],
-                        [[img_array[i+1][0]], [img_array[i+1][j+1]], [img_array[i+1][j+2]],
-                        [img_array[i+2][0]], [img_array[i+2][j+1]], [img_array[i+2][j+2]]]]
-        #print(tableau_temp2)
-        res = 0
-        NOMBRE_VOISIN = 7
-        for h in range(len(tableau_temp)):
-            for l in range(len(tableau_temp)):
-                valeur_pixel_voisin = tableau_temp[h][l]
+            tableau_temp2 = np.array([[img_array[i - 1][j - 1], img_array[i - 1][j], img_array[i - 1][j + 1]],
+                                      [img_array[i][j - 1], img_array[i][j], img_array[i][j + 1]],
+                                      [img_array[i + 1][j - 1], img_array[i + 1][j], img_array[i + 1][j + 1]]])
 
-            if valeur_pixel_voisin >= valeur_pixel_central:
-                s = 1
+            valeur_pixel_central = img_array[i][j]
 
-                res += 2**NOMBRE_VOISIN
+            if tableau_temp2[0][0] >= valeur_pixel_central:
+                nombre_voisin = 7
+                resultat += 2 ** nombre_voisin
 
-            else:
-                s = 0
+            if tableau_temp2[0][1] >= valeur_pixel_central:
+                nombre_voisin = 6
+                resultat += 2 ** nombre_voisin
 
-            NOMBRE_VOISIN -= 1
-        print(res)
-        tableau_temp [i,j] = res
+            if tableau_temp2[0][2] >= valeur_pixel_central:
+                nombre_voisin = 5
+                resultat += 2 ** nombre_voisin
 
-print(tableau_temp)
+            if tableau_temp2[1][2] >= valeur_pixel_central:
+                nombre_voisin = 4
+                resultat += 2 ** nombre_voisin
+
+            if tableau_temp2[2][2] >= valeur_pixel_central:
+                nombre_voisin = 3
+                resultat += 2 ** nombre_voisin
+
+            if tableau_temp2[2][1] >= valeur_pixel_central:
+                nombre_voisin = 2
+                resultat += 2 ** nombre_voisin
+
+            if tableau_temp2[2][0] >= valeur_pixel_central:
+                nombre_voisin = 1
+                resultat += 2 ** nombre_voisin
+
+            if tableau_temp2[1][0] >= valeur_pixel_central:
+                nombre_voisin = 0
+                resultat += 2 ** nombre_voisin
+
+            tableau_temp[i][j] = resultat
+            resultat = 0
+
+    print(np.array(img_array),end="\n"), print(tableau_temp)
+
+#test_appliquer_transformation_1_a([[2,5,3,9,15],[6,7,9,1,5],[3,8,4,2,9],[2,3,5,8,2],[1,2,3,2,1]])
+
+def test_appliquer_transformation_2_a(img_array):
+
+    print(test_appliquer_transformation_1_a(img_array))
+
+#test_appliquer_transformation_2_a([[88,20,56,49,145,123],[60,17,99,121,55,56],[80,8,45,100,99,30],[255,23,155,88,12,78],[100,200,23,82,155,254]])
+
+def test_appliquer_transformation_2_a(tableau, rayon):
+
+    print(appliquer_transformation_2(tableau, rayon))
+
+test_appliquer_transformation_2_a(np.array([[88,102,133,49,145,123],[14,100,200,121,55,56],[40,101,92,100,99,30],[255,23,155,88,12,78], [100,200,23,82,155,254]]), 1)
+def test_appliquer_transformation_2_b(tableau, rayon):
+
+    print(appliquer_transformation_2(tableau, rayon))
+
+test_appliquer_transformation_2_b(np.array([[88,102,133,49,145,123],[14,100,200,121,55,56],[40,101,92,100,99,30],[255,23,155,88,12,78], [100,200,23,82,155,254]]), 2)
--- traitement_image.py
+++ traitement_image.py
@@ -1,10 +1,10 @@
 # Importation du package Image de la bibliothèque Pillow
 from PIL import Image
 import numpy as np
+from math import log10
 
 """
-Cette fonction transforme une image en couleur en une nouvelle image
-en niveaux de gris.
+Cette fonction transforme une image en couleur en une nouvelle image en niveaux de gris.
 Arguments :
     chemin_image_couleur (str): Le chemin de l'image en couleur à transformer.
     chemin_sauvegarde_gris (str): Le chemin où sauvegarder l'image résultante en niveaux de gris.
@@ -41,55 +41,100 @@ def appliquer_rgb_to_gry(chemin_image_couleur, chemin_sauvegarde_gris):
     nouvelle_image.save(chemin_sauvegarde_gris)
 
 # Appel de la fonction en passant le chemin de l'image en argument
-#appliquer_rgb_to_gry('image_couleur.jpg', 'image_gris.jpg')
+#appliquer_rgb_to_gry('image_couleur.jpg', 'image_niveaux_de_gris.jpg')
 
+
+""""
+Cette fonction prend une image en niveaux de gris sous forme d'un tableau NumPy 2D et applique une transformation pour 
+simplifier et extraire des caractéristiques significatives de l'image.
+Arguments :
+    image_gris (numpy.ndarray): Un tableau 2D NumPy représentant une image en niveaux de gris.
+Retourne : 
+    numpy.ndarray: Un tableau 2D NumPy résultant de la transformation appliquée.
+"""
 
 def appliquer_transformation_1(image_gris):
 
     # Ouverture de l'image et conversion en tableau NumPy pour le traitement
-    img = Image.open(image_gris).convert('L')
-    img_array = np.array(img)
+    #img = Image.open(image_gris).convert('L')
+    img_array = image_gris
 
-    #Nombre constant de voisin égal à 8
-    NOMBRE_VOISIN = 8
-
-    #initialisation des variables
-    resultat = 0
-
-    image = np.array(image_gris)
+    #image = np.array(image_gris)
+    #img_array = image_gris
     # Obtenir les dimensions du tableau
     nombre_lignes = len(img_array)
     nombre_colonnes = len(img_array[0])
 
-    print(nombre_lignes, nombre_colonnes)
+    #initialisation des variables
+    tableau_temp = np.zeros((nombre_lignes, nombre_colonnes))
+    resultat = 0
+
     # Pour chaque pixel, on extrait ses valeurs des voisins (V=8).
 
-    for i in range(nombre_lignes):
+    for i in range(1, nombre_lignes - 1):
 
-        for j in range(nombre_colonnes):
+        for j in range(1, nombre_colonnes - 1):
+
+            tableau_temp2 = np.array([[img_array[i - 1][j - 1], img_array[i - 1][j], img_array[i - 1][j + 1]],
+                                      [img_array[i][j - 1], img_array[i][j], img_array[i][j + 1]],
+                                      [img_array[i + 1][j - 1], img_array[i + 1][j], img_array[i + 1][j + 1]]])
 
             valeur_pixel_central = img_array[i][j]
 
-            tableau_temp = [[img_array[i-1, j-1]],[img_array[i-1]], [img_array[i-1, j+1]],
-                            [img_array[i, j-1]],[img_array[i, j]], [img_array[i, j+1]],
-                            [img_array[i+1, j-1]], [img_array[i+1]], [img_array[i+1, j+1]]]
-            for h in range (len(tableau_temp)):
-                for l in range (len(tableau_temp)):
+            if tableau_temp2[0][0] >= valeur_pixel_central:
+                nombre_voisin = 7
+                resultat += 2 ** nombre_voisin
 
-                    valeur_pixel_voisin = tableau_temp[h][l]
+            if tableau_temp2[0][1] >= valeur_pixel_central:
+                nombre_voisin = 6
+                resultat += 2 ** nombre_voisin
 
-                if valeur_pixel_voisin >= valeur_pixel_central:
-                    s = 1
+            if tableau_temp2[0][2] >= valeur_pixel_central:
+                nombre_voisin = 5
+                resultat += 2 ** nombre_voisin
 
-                    tableau_temp[h][l] = 1
-                else:
-                    s = 0
-                    tableau_temp[h][l] = 0
+            if tableau_temp2[1][2] >= valeur_pixel_central:
+                nombre_voisin = 4
+                resultat += 2 ** nombre_voisin
 
-                    for v in range(NOMBRE_VOISIN):
+            if tableau_temp2[2][2] >= valeur_pixel_central:
+                nombre_voisin = 3
+                resultat += 2 ** nombre_voisin
 
-                        resultat += s(valeur_pixel_voisin - valeur_pixel_central)*2**v
+            if tableau_temp2[2][1] >= valeur_pixel_central:
+                nombre_voisin = 2
+                resultat += 2 ** nombre_voisin
 
+            if tableau_temp2[2][0] >= valeur_pixel_central:
+                nombre_voisin = 1
+                resultat += 2 ** nombre_voisin
 
+            if tableau_temp2[1][0] >= valeur_pixel_central:
+                nombre_voisin = 0
+                resultat += 2 ** nombre_voisin
 
-appliquer_transformation_1('image_gris.jpg')
+            tableau_temp[i][j] = resultat
+            resultat = 0
+    return tableau_temp
+
+#appliquer_transformation_1('image_gris.jpg')
+
+def appliquer_transformation_2(img_array, rayon):
+
+    # Obtenir les dimensions du tableau
+    nombre_lignes, nombre_colonnes = img_array.shape
+
+    # initialisation des variables
+    tableau_sortie = np.zeros((nombre_lignes, nombre_colonnes))
+
+    # Pour chaque pixel, on extrait ses valeurs des voisins (V=8).
+
+    for x in range(rayon, nombre_lignes - rayon):
+        for y in range(rayon, nombre_colonnes - rayon):
+            tableau_sortie[x, y] += np.log10(1.0 + np.abs(img_array[x, y + rayon] - 2 * img_array[x, y] + img_array[x, y - rayon]))
+            tableau_sortie[x, y] += np.log10(1.0 + np.abs(img_array[x + rayon, y] - 2 * img_array[x, y] + img_array[x - rayon, y]))
+            tableau_sortie[x, y] += np.log10(
+                1.0 + np.abs(img_array[x - rayon, y + rayon] - 2 * img_array[x, y] + img_array[x + rayon, y- rayon]))
+
+    matrice = tableau_sortie.astype(np.int32)
+    return matrice
